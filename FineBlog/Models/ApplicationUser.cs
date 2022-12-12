c3389b2ed028eb4959b7aedd46ee2cc2a9486c34
Sailesh Rijal <saileshrijal1@gmail.com>
2022-12-12 21:37:12 +0545

    Add project files.

---
+++ FineBlog/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FineBlog.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        //relation
+        public List<Post>? Posts { get; set; }
+    }
+}
---
+++ FineBlog/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace FineBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        //relation
+        public string? ApplicationUserId { get; set; }
+        public ApplicationUser? ApplicationUser { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string? Description { get; set; }
+        public string? Slug { get; set; }
+    }
+}
