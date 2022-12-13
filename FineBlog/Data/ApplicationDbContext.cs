f8db81dda9379836b9331dff1559e0ae7304fb0c
Sailesh Rijal <saileshrijal1@gmail.com>
2022-12-13 13:57:19 +0545

    database connection established

---
+++ FineBlog/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FineBlog.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FineBlog.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+        }
+        public DbSet<ApplicationUser>? ApplicationUsers { get; set; }
+        public DbSet<Post>? Posts { get; set; }
+        public DbSet<Page>? Pages { get; set; }
+    }
+}
