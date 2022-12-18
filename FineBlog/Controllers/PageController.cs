02ecc44c102569303b0d342c2fa92290d7a5b15c
Sailesh Rijal <saileshrijal1@gmail.com>
2022-12-18 13:22:54 +0545

    admin pannel ui added

---
+++ FineBlog/Controllers/PageController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FineBlog.Controllers
+{
+    public class PageController : Controller
+    {
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
--- FineBlog/Program.cs
+++ FineBlog/Program.cs
@@ -34,6 +34,10 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "area",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
