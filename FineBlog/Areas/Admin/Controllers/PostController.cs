e64abe5396084d29b5545c0ef42d6a259c596546
Sailesh Rijal <saileshrijal1@gmail.com>
2023-02-12 21:07:05 +0545

    post create functionality added

--- FineBlog/Areas/Admin/Controllers/PostController.cs
+++ FineBlog/Areas/Admin/Controllers/PostController.cs
@@ -1,6 +1,11 @@
-﻿using FineBlog.ViewModels;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using FineBlog.Data;
+using FineBlog.Models;
+using FineBlog.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FineBlog.Areas.Admin.Controllers
 {
@@ -8,6 +13,21 @@ namespace FineBlog.Areas.Admin.Controllers
     [Authorize]
     public class PostController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PostController(ApplicationDbContext context, 
+                                INotyfService notyfService, 
+                                IWebHostEnvironment webHostEnvironment,
+                                UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _notification = notyfService;
+            _webHostEnvironment = webHostEnvironment;
+            _userManager = userManager;
+        }
 
         public IActionResult Index()
         {
@@ -18,6 +38,53 @@ namespace FineBlog.Areas.Admin.Controllers
         public IActionResult Create()
         {
             return View(new CreatePostVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePostVM vm)
+        {
+            if(!ModelState.IsValid) { return View(vm); }
+
+            //get logged in user id
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+
+            var post = new Post();
+
+            post.Title = vm.Title;
+            post.Description = vm.Description;
+            post.ShortDescription = vm.ShortDescription;
+            post.ApplicationUserId = loggedInUser!.Id;
+
+            if(post.Title!= null)
+            {
+                string slug = vm.Title!.Trim();
+                slug = slug.Replace(" ", "-");
+                post.Slug = slug + "-" + Guid.NewGuid();
+            }
+
+            if (vm.Thumbnail != null)
+            {
+                post.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+
+            await _context.Posts!.AddAsync(post);
+            await _context.SaveChangesAsync();
+            _notification.Success("Post Created Successfully");
+            return RedirectToAction("Index");
+        }
+
+
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = "";
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filePath = Path.Combine(folderPath, uniqueFileName);
+            using(FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fileStream);
+            }
+            return uniqueFileName;
         }
 
     }
--- FineBlog/ViewModels/CreatePostVM.cs
+++ FineBlog/ViewModels/CreatePostVM.cs
@@ -1,10 +1,12 @@
 ﻿using FineBlog.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace FineBlog.ViewModels
 {
     public class CreatePostVM
     {
         public int Id { get; set; }
+        [Required]
         public string? Title { get; set; }
         public string? ShortDescription { get; set; }
         public string? ApplicationUserId { get; set; }
