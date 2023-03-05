5c8045e38dc315f91d7849bd3087468c71b6077e
Sailesh Rijal <saileshrijal1@gmail.com>
2023-03-05 08:20:43 +0545

    pages module completed

---
+++ FineBlog/Migrations/20230305021726_thumbnailAddedOnPage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FineBlog.Migrations
+{
+    public partial class thumbnailAddedOnPage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailUrl",
+                table: "Pages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThumbnailUrl",
+                table: "Pages");
+        }
+    }
+}
--- FineBlog/ViewModels/PageVM.cs
+++ FineBlog/ViewModels/PageVM.cs
@@ -1,12 +1,15 @@
-﻿namespace FineBlog.Models
+﻿using Microsoft.Build.Framework;
+
+namespace FineBlog.ViewModels
 {
-    public class Page
+    public class PageVM
     {
         public int Id { get; set; }
+        [Required]
         public string? Title { get; set; }
         public string? ShortDescription { get; set; }
         public string? Description { get; set; }
-        public string? Slug { get; set; }
         public string? ThumbnailUrl { get; set; }
+        public IFormFile? Thumbnail { get; set; }
     }
 }
