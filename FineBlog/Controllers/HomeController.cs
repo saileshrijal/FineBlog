6c7235e0067408a3a4c19d8c6f6d01d8774e5e17
Sailesh Rijal <saileshrijal1@gmail.com>
2023-03-15 21:12:28 +0545

    home page design completed

--- FineBlog/Controllers/HomeController.cs
+++ FineBlog/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using FineBlog.Models;
+﻿using FineBlog.Data;
+using FineBlog.Models;
+using FineBlog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace FineBlog.Controllers
@@ -7,15 +10,24 @@ namespace FineBlog.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, 
+                                ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var vm = new HomeVM();
+            var setting = _context.Settings!.ToList();
+            vm.Title = setting[0].Title;
+            vm.ShortDescription = setting[0].ShortDescription;
+            vm.ThumbnailUrl = setting[0].ThumbnailUrl;
+            vm.Posts = _context.Posts!.Include(x=>x.ApplicationUser).ToList();
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ FineBlog/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using FineBlog.Models;
+
+namespace FineBlog.ViewModels
+{
+    public class HomeVM
+    {
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public List<Post>? Posts { get; set; }
+    }
+}
